2fcda5ae975c950765778659beb43d4bff427048
Jawor Iwanow <jaworiwanow@gmx.net>
2022-12-04 14:55:43 +0100

    updated imports and methods

--- src/helium_positioning_api/Models.py
+++ src/helium_positioning_api/Models.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from typing import List
 from helium_positioning_api.DataObjects import Prediction, Hotspot
-from helium_api_wrapper.helpers import load_last_integration
+from helium_api_wrapper.devices import get_last_integration
 from helium_positioning_api.auxilary import midpoint
 
 
@@ -15,8 +15,8 @@ class Model:
 
     def get_hotspots(self, uuid: str) -> List[Hotspot]:
         """Load hotspots, which interacted with the given device from the last integration event."""
-        integration = load_last_integration(uuid)
-        return [Hotspot(**h) for h in integration["data"]["req"]["body"]["hotspots"]]
+        integration = get_last_integration(uuid)
+        return [Hotspot(**h) for h in integration.data["req"]["body"]["hotspots"]]
         
 
 
@@ -42,7 +42,7 @@ class Midpoint(Model):
     def predict(self, uuid: str) -> Prediction:
         hotspots = self.get_hotspots(uuid)
         sorted_hotspots = sorted(hotspots, key=lambda h: h.rssi)
-
+        assert len(sorted_hotspots) > 1, "Not enough witnesses"
         midpoint_lat, midpoint_long = midpoint(sorted_hotspots[0], sorted_hotspots[1])
 
         return Prediction(uuid= uuid, lat = midpoint_lat, lng = midpoint_long)
