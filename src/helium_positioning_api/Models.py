de1597b24d8778df8ecf6d880da9165654c58042
Jawor Iwanow <jaworiwanow@gmx.net>
2022-12-07 16:17:01 +0100

    Co-authored-by: BerndSaurugger <BerndSaurugger@users.noreply.github.com>
    Co-authored-by: hexann666
     <ms.afedorova@gmail.com>

--- src/helium_positioning_api/Models.py
+++ src/helium_positioning_api/Models.py
@@ -1,11 +1,12 @@
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod
 from typing import List
-from helium_positioning_api.DataObjects import Prediction, Hotspot
+from helium_positioning_api.DataObjects import Prediction
+from helium_positioning_api.DataObjects import Hotspot
 from helium_api_wrapper.devices import get_last_integration
 from helium_positioning_api.auxilary import midpoint
 
 
-class Model:
+class Model():
     """Base Model class."""
 
     @abstractmethod
@@ -14,16 +15,21 @@ class Model:
         pass
 
     def get_hotspots(self, uuid: str) -> List[Hotspot]:
-        """Load hotspots, which interacted with the given device from the last integration event."""
+        """Load interacting hotspots from last integration event."""
         integration = get_last_integration(uuid)
         return [Hotspot(**h)
                 for h in integration.data["req"]["body"]["hotspots"]]
 
 
 class NearestNeighborModel(Model):
-    """This model predicts the location of a given device, by taking the location of the nearest witness in terms of highest rssi recieved."""
+    """This model predicts the location of a given device.
+
+    It takes the location of the nearest witness
+    in terms of highest rssi recieved.
+    """
 
     def __init__(self) -> None:
+        """Initialize an object of Class NearestNeighborModel."""
         pass
 
     def predict(self, uuid: str) -> Prediction:
@@ -37,12 +43,17 @@ class NearestNeighborModel(Model):
         sorted_hotspots = sorted(hotspots, key=lambda h: h.rssi)
         nearest_neighbor = sorted_hotspots[0]
         nearest_neighbor.load_location()
-        return Prediction(uuid=uuid, lat=nearest_neighbor.lat, lng=nearest_neighbor.long)
+        return Prediction(
+            uuid=uuid, lat=nearest_neighbor.lat, lng=nearest_neighbor.long
+        )
+
 
 class Midpoint(Model):
-    """This model predicts the location of a given device, by approximating the midpoint of the two witnesses with the highest rssi."""
- 
+    """This model predicts the location of a given device. \
+    It approximates the midpoint of the two witnesses with the highest rssi."""
+
     def __init__(self) -> None:
+        """Initialize an object of class Midpoint."""
         pass
 
     def predict(self, uuid: str) -> Prediction:
@@ -57,19 +68,4 @@ class Midpoint(Model):
         assert len(sorted_hotspots) > 1, "Not enough witnesses"
         midpoint_lat, midpoint_long = midpoint(
             sorted_hotspots[0], sorted_hotspots[1])
-
         return Prediction(uuid=uuid, lat=midpoint_lat, lng=midpoint_long)
-        
-# class SimpleTrilateration(Model):
-#     """This model predicts the location of a given device, 
-# by trilateration of the three witnesses with the highest rssi."""
-#     def __init__(self) -> None:
-#         pass
-
-#     def predict(self, uuid: str) -> Prediction:
-#         hotspots = self.get_hotspots(uuid)
-#         sorted_hotspots = sorted(hotspots, key=lambda h: h.rssi)
-#         # defining centres
-#         sorted_hotspots[0].load_location()
-#         sorted_hotspots[1].load_location()
-#         sorted_hotspots[2].load_location()
