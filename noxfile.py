5e8b776d9e5abb8811a78d2dce61c3ae33c109c3
Jawor Iwanow <jaworiwanow@gmx.net>
2022-12-09 23:16:43 +0100

    trying to make tests work

--- noxfile.py
+++ noxfile.py
@@ -132,6 +132,7 @@ def precommit(session: Session) -> None:
         "pre-commit",
         "pre-commit-hooks",
         "pyupgrade",
+        "haversine",
     )
     session.run("pre-commit", *args)
     if args and args[0] == "install":
