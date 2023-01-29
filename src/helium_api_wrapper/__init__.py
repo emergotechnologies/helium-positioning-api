fe28b206eb36072e6496ef5aeadd0efad6694a45
Fabian Kainz <fabian@kainz.in>
2023-01-29 19:19:54 +0100

    Merge branch 'feature/model_device' of https://github.com/emergotechnologies/helium-positioning-api into feature/model_device
    Co-Authored-By: BerndSaurugger <96190267+BerndSaurugger@users.noreply.github.com>

---
+++ src/helium_api_wrapper/__init__.py
@@ -0,0 +1,14 @@
+"""Helium Api Wrapper.
+
+.. module:: helium_api_wrapper
+
+:synopsis: Command-line interface
+
+.. moduleauthor:: DSIA21
+
+"""
+import __main__
+
+from helium_api_wrapper import challenges as challenges
+from helium_api_wrapper import devices as devices
+from helium_api_wrapper import hotspots as hotspots
